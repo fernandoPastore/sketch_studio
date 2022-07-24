f1e7e4a49359b7453da7f50da89858a73483f201
HenriqueePereira <https://github.com/HenriqueePereira>
2022-07-23 23:13:21 -0300

    teste

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,69 +1,96 @@
 const bcrypt = require('bcrypt');
-const {User} = require('../models');
+const {
+    User
+} = require('../models');
 
 const AuthController = {
-    showRegistro:(req,res)=> {
+    showRegistro: (req, res) => {
         res.render('registro')
-        },
-    
-    Registrar: async(req,res)=>{
-            try {
-                const {nome, email, senha} = req.body;
-    
-                const hash = bcrypt.hashSync(senha,10);
-    
-                const verificarUsuarioCadastrado = await User.findOne({where: {email:email}});
-                
-                if(verificarUsuarioCadastrado){
-    
-                    return res.render('registro', {erro:"Não foi possível realizar o Cadastro", old: req.body})   
+    },
+
+    Registrar: async (req, res) => {
+        try {
+            const {
+                nome,
+                email,
+                senha
+            } = req.body;
+
+            const hash = bcrypt.hashSync(senha, 10);
+
+            const verificarUsuarioCadastrado = await User.findOne({
+                where: {
+                    email: email
                 }
-            
-                const newUser = await User.create(
-                    {
-                        nome,
-                        email,
-                        senha: hash,
-                    })
-    
-            return res.redirect('/login')
-    
-            } catch (error){
-                console.log(error)
+            });
+
+            if (verificarUsuarioCadastrado) {
+
+                return res.render('registro', {
+                    erro: "Não foi possível realizar o Cadastro",
+                    old: req.body
+                })
             }
-            
-        },
-        
-    showLogin: (req,res)=> {
-           res.render('login.ejs',{})
-        },
-    
-    Login: async (req,res)=> {
-            try{
-            const {email,senha} = req.body
-    
-            let user = await User.findOne({where: {email: email}})
-    
-            if(!user || bcrypt.compareSync(senha,user.senha)){
-                return res.render('login', {error:'Usuário e senha incorretos ou não existe'})
+
+            const newUser = await User.create({
+                nome,
+                email,
+                senha: hash,
+            })
+
+            return res.redirect('/login')
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    },
+
+    showLogin: (req, res) => {
+        res.render('login.ejs', {})
+    },
+
+    Login: async (req, res) => {
+        try {
+            const {
+                email,
+                senha
+            } = req.body
+
+            let user = await User.findOne({
+                where: {
+                    email: email
+                }
+            })
+
+            if (!user || bcrypt.compareSync(senha, user.senha)) {
+                return res.render('login', {
+                    error: 'Usuário e senha incorretos ou não existe'
+                })
             }
 
             req.session.user = user
 
             res.redirect('/homePage')
-            }catch (error){
-                console.log(error)
-            }
-    
-        },
-    
-    showResetSenha:(req,res)=> {
-            res.render('resetSenha')
-        },
-    
-    showHomePage: (req,res)=> {
-            res.render('homePage')  
-        },
+        } catch (error) {
+            console.log(error)
+        }
+
+    },
+
+    showResetSenha: (req, res) => {
+        res.render('resetSenha')
+    },
+
+    showHomePage: (req, res) => {
+        res.render('homePage')
+    },
+    showComentsPage: (req, res) => {
+        res.render("comentsPage")
+    },
+    showSobreNos: (req, res) => {
+        res.render('sobreNos')
+    },
 }
 
 module.exports = AuthController;
